afd02a1930a16f38681bf7d12fd9c865877a3151
vivekCometChat <vivek.prajapati@cometchat.com>
2026-03-20 21:31:58 +0530

    fix(test): mock IntersectionObserver in tests to fix CI build

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,21 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock IntersectionObserver for components using scroll reveal animations
+class MockIntersectionObserver {
+  constructor(callback, options) {}
+  observe(element) {}
+  unobserve(element) {}
+  disconnect() {}
+}
+window.IntersectionObserver = MockIntersectionObserver;
+
+// Mock matchMedia
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+};
